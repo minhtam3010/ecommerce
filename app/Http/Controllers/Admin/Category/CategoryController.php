2dd50903380de2c4b7dbdc681cb2d71e4266a095
unknown <minhtam30102002@gmail.com>
2022-07-07 23:11:39 +0700

    Tam Commit

--- app/Http/Controllers/Admin/Category/CategoryController.php
+++ app/Http/Controllers/Admin/Category/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\Category;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Model\Admin\Category;
 
 class CategoryController extends Controller
 {
@@ -12,7 +13,13 @@ class CategoryController extends Controller
         $this->middleware('auth:admin');
     }
 
-    
+    public function category(){
+        
+        $category = Category::all();
+        return view('admin.category.category', compact('category'));
+    }
 
-
+    public function storecategory(Request $request){
+        
+    }
 }
